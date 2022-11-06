0c5d4b3f8e80490ed897056fd1c63a95c06ecc3a
Luz1202 <luzarevalo@ufm.edu>
2022-11-06 13:26:29 -0600

    Today

---
+++ Proyecto_shiny/shiny/server.R
@@ -0,0 +1,74 @@
+library(shiny)
+library(crosstalk)
+library(DT)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+shinyServer(function(input, output, session) {
+  athlete_events <<- read.csv("athlete_events.csv")
+  
+  #### EVENTOS ####
+  Events <- athlete_events %>%
+    distinct(City, Year, Season) %>%
+    arrange(Year)
+  
+  output$tablaEventos <- renderDataTable({
+    dt <- Events %>%
+      filter(Year >= input$ChooseYear[1] & Year <= input$ChooseYear[2]) %>%
+      filter(City %in% input$selectHost) %>%
+      filter(Season %in% input$chkboxSeason)
+    dt
+  })
+  
+  observeEvent(input$clean,{
+    updatePickerInput(session, 'selectHost', choices = unique(sort(athlete_events$City)),options = list(`actions-box` = TRUE))
+    updateSliderInput(session, 'ChooseYear', value = c(min(athlete_events$Year), max(athlete_events$Year)))
+    updateCheckboxGroupInput(session, 'chkboxSeason', choices = unique(athlete_events$Season), selected=NULL, inline = TRUE)
+  })
+  
+  #### ATLETAS ####
+  sex <<- NULL
+  age <<- NULL
+  
+  atletas <- reactive({
+    if(!is.null(input$season)&!is.null(input$year)&!is.null(input$sport)){
+      sex <<- athlete_events%>%
+        select(Season,Year,Sport,Sex)%>%
+        filter(Season == input$season & Year == input$year & Sport == input$sport)
+      
+      age <<- athlete_events%>%
+        select(Season,Year,Sport,Age)%>%
+        filter(Season == input$season & Year == input$year & Sport == input$sport)
+    }
+  })
+  
+  output$plotSexo <- renderPlot({
+    atletas()
+    if (nrow(sex)!=0){
+      barplot(table(sex$Sex), main = "Cantidad de atletas",
+              names.arg = c("Mujeres","Hombres"), col = c("pink","lightblue"),
+              horiz = TRUE)
+    }
+  })
+  
+  output$plotEdades <- renderPlot({
+    atletas()
+    if (nrow(age)!=0){
+      hist(age$Age, main = "Distribución de edades", 
+           xlab = "Edad", col = "lightblue", 
+           breaks = seq(min(age$Age), max(age$Age), length.out = 6))
+    }
+  })
+  
+  Atletas <- athlete_events %>%
+    distinct(Name, Games)
+  Atletas
+  
+  countAtletas <- Atletas %>%
+    select(Name, Games) %>%
+    group_by(Name)%>%
+    summarise(participacion = n_distinct(Games))
+  countAtletas
+  
+})
---
+++ Proyecto_shiny/shiny/ui.R
@@ -0,0 +1,79 @@
+library(shiny)
+library(shinythemes)
+library(markdown)
+library(lubridate)
+library(shinyWidgets)
+
+shinyUI(fluidPage(theme = shinytheme("sandstone"),
+                  
+                  navbarPage("Juegos Olimpicos",
+                             tabPanel("Eventos", icon = icon("fa-light fa-calendar-star"),
+                                      sidebarLayout(
+                                        sidebarPanel(
+                                          h2('Eventos Olimpicos'),
+                                          h4('Complete los 3 filtros para ver su informacion'),
+                                          br(),
+                                          sliderInput('ChooseYear', 'Seleccione Rango de Años:',
+                                                      value = c(min(athlete_events$Year), max(athlete_events$Year)),sep = "", 
+                                                      min = min(athlete_events$Year), max = max(athlete_events$Year),
+                                                      step = 2),
+                                          br(),
+                                          pickerInput('selectHost', 'Seleccione Ciudad Host:',
+                                                      choices = unique(sort(athlete_events$City)),
+                                                      options = list(`actions-box` = TRUE),
+                                                      multiple = T),
+                                          br(),
+                                          checkboxGroupInput('chkboxSeason', 'Seleccione Temporada:',
+                                                             choices = unique(athlete_events$Season),
+                                                             selected = NULL, inline = TRUE),
+                                          br(),
+                                          actionButton("clean","Limpiar")
+                                        ),
+                                        mainPanel(
+                                          dataTableOutput("tablaEventos")
+                                        )
+                                      )
+                             ),
+                             tabPanel("Equipos",icon = icon("fa-thin fa-users-viewfinder"),
+                             ),
+                             tabPanel("Atletas", icon = icon("fa-thin fa-ranking-star"),
+                                      sidebarLayout(
+                                        sidebarPanel(width = 3,
+                                                     h2('Atletas participantes'),
+                                                     br(),
+                                                     checkboxGroupInput('season','Season',choices = unique(athlete_events$Season), 
+                                                                        selected = unique(athlete_events$Season)),
+                                                     numericInput('year','Year',value = 2000, step = 2, min = min(athlete_events$Year), 
+                                                                  max = max(athlete_events$Year)),
+                                                     selectInput('sport', 'Sport', choices = unique(athlete_events$Sport),selected = athlete_events$Sport[1]),
+                                                     submitButton(text = "Aply",icon = icon("fa-light fa-fire-flame-curved"), width = 100)
+                                        ),
+                                        mainPanel(
+                                          tabsetPanel(
+                                            tabPanel(
+                                              "Informacion General",
+                                              h2("Informacion Atletas"),
+                                              fluidRow(
+                                                column(12, dataTableOutput('tablaAtletas'))
+                                              )
+                                            ),
+                                            tabPanel(
+                                              "Edad y Sexo",
+                                              fluidRow(
+                                                column(6, 
+                                                       h4("Grafica Sexo"),
+                                                       plotOutput('plotSexo')),
+                                                column(6,
+                                                       h4("Grafica Edades"),
+                                                       plotOutput('plotEdades'))
+                                              )
+                                            )
+                                          )
+                                        )
+                                      )
+                             ),
+                             tabPanel("Logros", icon = icon("fa-duotone fa-medal"),
+                             )
+                  )
+)
+)
